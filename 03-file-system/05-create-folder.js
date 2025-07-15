da8785468ca13b2440793fe537f640d0b219bd71
Clinton Moshe <clintonmwachia9@gmail.com>
2025-07-15 16:40:29 +0300

    ADDED Create a folder docs

--- 03-file-system/05-create-folder.js
+++ 03-file-system/05-create-folder.js
@@ -2,5 +2,5 @@ const fs = require("node:fs");
 
 fs.mkdir("logs", { recursive: true }, (err) => {
   if (err) return console.error("Error creating folder:", err.message);
-  console.log("logs folder created.");
+  console.log("logs folder created");
 });

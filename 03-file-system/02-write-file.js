249ce123486f254511b7fe6923a76d798ba35093
Clinton Moshe <clintonmwachia9@gmail.com>
2025-07-15 16:34:05 +0300

    ADDED Write to a file docs

---
+++ 03-file-system/02-write-file.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+const content = "This is a new file created using fs.writeFile.";
+
+fs.writeFile("newfile.txt", content, (err) => {
+  if (err) {
+    console.error("Error writing file:", err.message);
+    return;
+  }
+  console.log("newfile.txt created successfully.");
+});

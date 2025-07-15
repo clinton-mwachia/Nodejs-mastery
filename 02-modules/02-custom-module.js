192366db05f92447c09e62b453ada206ec535103
Clinton Moshe <clintonmwachia9@gmail.com>
2025-07-15 16:14:40 +0300

    ADDED Custom module docs

---
+++ 02-modules/02-custom-module.js
@@ -0,0 +1,4 @@
+// Using a custom module
+const greet = require("./03-custom-module");
+
+greet("Clinton");
---
+++ 02-modules/03-custom-module.js
@@ -0,0 +1,6 @@
+// This is a custom module
+function greet(name) {
+  console.log(`Hello, ${name}! Welcome to Node.js modules.`);
+}
+
+module.exports = greet;

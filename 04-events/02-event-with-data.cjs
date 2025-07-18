8312889ef7fe90a82ac75b5a4956cb1bec77b258
Clinton Moshe <clintonmwachia9@gmail.com>
2025-07-18 16:53:02 +0300

    ADDED Event emitter with multiple data example

--- 04-events/02-event-with-data.cjs
+++ 04-events/02-event-with-data.cjs
@@ -2,7 +2,16 @@ const EventEmitter = require("node:events");
 const emitter = new EventEmitter();
 
 emitter.on("userLogin", (username) => {
-  console.log(`${username} has logged in.`);
+  console.log(`${username}: has logged in.`);
 });
 
-emitter.emit("userLogin", "clinton");
+emitter.on("userLogin", (...args) => {
+  const parameters = args.join(", ");
+  console.log(`user details: ${parameters}`);
+});
+
+//console.log(emitter.listeners("userLogin"));
+
+//emitter.emit("userLogin", "clinton");
+
+emitter.emit("userLogin", "clinton", "moshe", 10);
